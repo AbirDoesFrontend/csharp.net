1216e013b0ba926cfd228bd1d3dec22712c4c048
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2025-06-17 22:17:39 +0600

    Converting String to Numbers

---
+++ Text_Based_Data_Types/Text_Based_Data_Types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_Data_Types
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            string name = "Abir Khan";
+            char c = 'a';
+            Console.WriteLine(name);
+            Console.WriteLine(c);
+        }
+    }
+}
