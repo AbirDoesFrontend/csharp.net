74db0b64367e17bffa8331c2551de6069d67993e
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2025-06-18 23:23:13 +0600

    Refactor project structure and update configuration files
    - Improved project organization by restructuring directories.
    - Updated .gitignore to better exclude unnecessary files.
    - Enhanced IDE configuration with additional settings for improved usability.

---
+++ Boolean_Data_Type/Boolean_Data_Type/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Data_Type 
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var isMale = true;
+            Console.WriteLine("Is Male? {0}", isMale);
+            
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            double age = 23;
+            age /= 10;
+            Console.WriteLine(age);
+
+            string name = "Abir";
+            name += " is learning C#";
+            Console.WriteLine(name);
+            
+            char c = 'a';
+            c += 'b';
+            Console.WriteLine(c);
+
+            int i = 0;
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+            
+            Console.ReadLine();
+        }
+    }
+}
